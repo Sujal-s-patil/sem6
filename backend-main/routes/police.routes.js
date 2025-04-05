c15157c1123010f739ca9c11432119dcce5ffa5e
Sujal-s-patil <153197090+Sujal-s-patil@users.noreply.github.com>
2025-04-05 20:49:51 +0530

    Update police.routes.js

--- backend-main/routes/police.routes.js
+++ backend-main/routes/police.routes.js
@@ -1,38 +1,17 @@
 const express = require("express");
 
 const policeRouter = express.Router();
-const { policeLogin,policeRegister,getTeamInfo,specificPolice,assignPolice} = require("../controllers/police.controller.js")
+const { policeLogin,policeRegister,getTeamInfo,specificPolice,assignPolice,unassignPolice,getUserById} = require("../controllers/police.controller.js")
 
-/* policeRouter.get("/", getPoliceInfos);
-policeRouter.get("/:username", getPoliceInfo); */
 
 policeRouter.get("/team",getTeamInfo);
 policeRouter.post("/login",policeLogin);
 policeRouter.post("/register",policeRegister);
 policeRouter.post("/specific",specificPolice);
 policeRouter.put("/assign",assignPolice);
+policeRouter.put("/unassign",unassignPolice);
+policeRouter.get("/:id",getUserById);
+
 
 
 module.exports =  policeRouter;
-
-
-/* 
-
-  {
-    "police_id": "sujal12345",
-    "password": "djrajesh",
-    "photo": null,
-    "aadhar_card": 123456789,
-    "full_name": "sujal santosh patil",
-    "phone_no": null,
-    "email": null,
-    "address": null,
-    "city": "bhiwandi",
-    "state": "maharashra",
-    "blood_group": null,
-    "post": "bhiwandi",
-    "speciality": null,
-    "description": null
-  }
-
-*/
