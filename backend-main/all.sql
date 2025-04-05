59c5e6d56058c18790954d0ce4561ca426082d9d
Sujal-s-patil <153197090+Sujal-s-patil@users.noreply.github.com>
2025-04-05 20:47:42 +0530

    Update all.sql

--- backend-main/all.sql
+++ backend-main/all.sql
@@ -24,24 +24,26 @@ CREATE TABLE
         gender VARCHAR(10) DEFAULT 'Male' -- Male or Female
     );
 
-CREATE TABLE
-    police (
-        police_id VARCHAR(50) primary key,
-        photo VARCHAR(255) DEFAULT NULL,
-        aadhar_card BIGINT,
-        password varchar(255),
-        full_name VARCHAR(255),
-        phone_no bigint DEFAULT NULL,
-        email VARCHAR(255) DEFAULT NULL,
-        address TEXT DEFAULT NULL,
-        city varchar(255),
-        state VARCHAR(255),
-        blood_group varchar(10) DEFAULT NULL,
-        post VARCHAR(255),
-        speciality VARCHAR(255) DEFAULT NULL,
-        description TEXT DEFAULT NULL,
-        gender VARCHAR(10) DEFAULT 'Male' -- Male or Female
-    );
+CREATE TABLE police (
+    police_id VARCHAR(50) PRIMARY KEY,
+    photo VARCHAR(255) DEFAULT NULL,
+    aadhar_card BIGINT,
+    password VARCHAR(255),
+    full_name VARCHAR(255),
+    phone_no BIGINT DEFAULT NULL,
+    email VARCHAR(255) DEFAULT NULL,
+    address TEXT DEFAULT NULL,
+    city VARCHAR(255),
+    state VARCHAR(255),
+    blood_group VARCHAR(10) DEFAULT NULL,
+    post VARCHAR(255),
+    speciality VARCHAR(255) DEFAULT NULL,
+    description TEXT DEFAULT NULL,
+    gender VARCHAR(10) DEFAULT 'Male', -- Male or Female
+    occupied BOOLEAN DEFAULT FALSE,
+    complaint_id INT DEFAULT NULL
+);
+    
 
 CREATE TABLE
     public (
